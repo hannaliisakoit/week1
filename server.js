a74612afad8b57c6b028b2dd5811682056d338ff
Hanna-Liisa Koit <hkoit@tktk.ee>
2023-09-09 11:44:25 +0300

    week 1 code

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 let firstName = 'Harry';
 let lastName = "Potter";
 //template literals
-//console.log(`Hello, ${firstName}!`) ;
+//console.log(`Hello, ${firstName}!`);
 
 let greeting = `Hello, ${firstName} ${lastName}!`;
 console.log(greeting);
